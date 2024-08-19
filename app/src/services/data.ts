f9da193d1fa05268c27cfe4d7de002953affe3dc
Brian Jacobel <bjacobel@gmail.com>
2024-08-18 22:00:23 -0400

    remove mock data service

--- app/src/services/data.ts
+++
@@ -1,6 +0,0 @@
-import { DATA } from '../constants';
-
-export type Data = typeof DATA;
-
-// eslint-disable-next-line import/prefer-default-export
-export const getData = () => Promise.resolve(DATA);
