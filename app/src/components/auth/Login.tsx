acdcdf41585f40f326838d9482b44d43954351fb
Brian Jacobel <bjacobel@gmail.com>
2024-10-19 16:36:33 -0400

    Login page is better styled and handles redirection

--- app/src/components/auth/Login.tsx
+++ app/src/components/auth/Login.tsx
@@ -1,24 +1,55 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
-import { useLocation } from 'wouter';
+import { useLocation, useSearch } from 'wouter';
 
 import { useSupabase } from '../../contexts/Supabase';
 import useSupabaseSession from '../../hooks/useSupabaseSession';
 import Loading from '../Loading';
+import PrimaryLayout from '../PrimaryLayout';
+import { blue } from '../../stylesheets/colors.css';
 
 export default () => {
   const supabase = useSupabase();
   const { loading, session } = useSupabaseSession();
   const [__location, setLocation] = useLocation();
+  const search = useSearch();
+
+  const getNextUrl = useCallback(() => {
+    if (search) {
+      const searchParams = new URLSearchParams(search);
+      if (searchParams.has('next')) {
+        return decodeURIComponent(searchParams.get('next')!);
+      }
+    }
+    return '/box';
+  }, [search]);
 
   useEffect(() => {
     if (!loading && session) {
-      setLocation('/box');
+      setLocation(getNextUrl());
     }
-  }, [session, loading, setLocation]);
+  }, [session, loading, setLocation, getNextUrl]);
 
   if (!supabase || loading) return <Loading />;
 
-  return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]} />;
+  return (
+    <PrimaryLayout hideMenu>
+      <Auth
+        supabaseClient={supabase}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: blue.color,
+                brandAccent: blue.color,
+              },
+            },
+          },
+        }}
+        providers={[]}
+      />
+    </PrimaryLayout>
+  );
 };
